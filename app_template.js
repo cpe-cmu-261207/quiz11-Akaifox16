b22d5c9b646fff52a9c321670aa21ca5b170ac7d
KAMONPAT SUNTHONPONG <kamonpat_s@cmu.ac.th>
2021-01-12 13:56:54 +0700

    ข้อแรก

--- app_template.js
+++ app_template.js
@@ -1,1 +1,16 @@
 //your code here!
+document.addEventListener('DOMContentLoaded',function(){
+    document.querySelector("#button-compute").onclick = (e)=>{
+            const minusstdID = document.querySelector("#input-number").value
+            let result = document.querySelector("#compute-result")
+            result.innerHTML = 620610771 -  Number(minusstdID)
+            console.log(result)
+            return 0
+        }
+    
+    document.getElementById("button-create-table").onclick = () =>{
+        document.getElementById("table")
+        const subjCol = document.createElement('td')
+        const row = document.createElement("tr")
+    }
+})
